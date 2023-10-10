811c3b45c6f7e7e6fcd92ec859ec8db5db6df4f8
Abukstech <abdulmajeedbamidele4@gmail.com>
2023-10-10 14:58:05 +0100

    Build nav bar

---
+++ app/NavBar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-6 h-14 items-center">
+      <Link href="/">Logo</Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
