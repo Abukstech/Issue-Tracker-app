008cb671af3e3e247fd09198c71245c440f22846
Abukstech <abdulmajeedbamidele4@gmail.com>
2023-10-27 08:48:02 +0100

    Show issue details

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,17 +1,14 @@
+import { Table, TableColumnHeaderCell } from "@radix-ui/themes";
 import React from "react";
-import { Button, Table, TableColumnHeaderCell } from "@radix-ui/themes";
-import Link from "next/link";
-import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import IssuesActions from "./IssuesActions";
 
-const Issuespage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(2000);
-
+const IssueLoadingSkeleton = () => {
+  const issues = [1, 2, 3, 4, 5];
   return (
-    <div>
+    <>
       <IssuesActions />
       <Table.Root variant="surface">
         <Table.Header>
@@ -28,26 +25,25 @@ const Issuespage = async () => {
         <Table.Body>
           {issues &&
             issues.map((issue) => (
-              <Table.Row key={issue.id}>
+              <Table.Row key={issue}>
                 <Table.Cell>
-                  <Link href={`issues/${issue.id}`}>{issue.title}</Link>
-
+                  <Skeleton />
                   <div className="block md:hidden">
-                    <IssueStatusBadge status={issue.status} />
+                    <Skeleton />
                   </div>
                 </Table.Cell>
                 <Table.Cell className="hidden md:table-cell">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </Table.Cell>
                 <Table.Cell className="hidden md:table-cell">
-                  {issue.createdAt.toDateString()}
+                  <Skeleton />
                 </Table.Cell>
               </Table.Row>
             ))}
         </Table.Body>
       </Table.Root>
-    </div>
+    </>
   );
 };
 
-export default Issuespage;
+export default IssueLoadingSkeleton;
